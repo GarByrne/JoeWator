72a2403095b6c9318deb7fa717b7cd0bfafcb675
GarByrne <1garry8@gmail.com>
2018-01-06 22:09:39 +0000

    FDASfsa

---
+++ fish.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class Fish
+{
+
+private:
+	int turn;
+
+public:
+	Fish();
+	~Fish();
+	int move(int i, int j);
+	bool checkOpen(int topLeft, int top, int topRight, int right, int bottomRight, int bottom, int bottomLeft, int left);
+};
